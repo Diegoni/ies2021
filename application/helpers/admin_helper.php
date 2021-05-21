6d74c34cf48a53027b7f73927c8a7dd37164720e
Diego Nieto <diego_nieto_1@hotmail.com>
2021-05-21 18:58:59 -0300

    conectando adminLTE 3

---
+++ application/helpers/admin_helper.php
@@ -0,0 +1,14 @@
+<?php
+
+function setJsAdmin($js) {
+	return '<script src="assets/AdminLTE-3.1.0/'.$js.'"></script>';
+}
+
+function setCssAdmin($css) {
+	return '<link rel="stylesheet" href="assets/AdminLTE-3.1.0/'.$css.'">';
+}
+
+function setImgAdmin($img) {
+	return "assets/AdminLTE-3.1.0/".$img;
+}
+?>
