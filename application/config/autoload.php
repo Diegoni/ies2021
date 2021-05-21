87ca9edad8fc913022b40bb606c5f03d4388f630
Diego Nieto <diego_nieto_1@hotmail.com>
2021-05-21 19:19:35 -0300

    ejemplo con libros

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('admin');
+$autoload['helper'] = array('admin', 'url');
 
 /*
 | -------------------------------------------------------------------
--- application/core/My_Controller.php
+++ application/core/My_Controller.php
@@ -3,4 +3,10 @@ class MY_Controller extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 	}
+
+	public function setView($view, $db = null){
+		$this->load->view('admin/menu.php', $db);
+		$this->load->view($view);
+		$this->load->view('admin/footer.php');
+	}
 }
